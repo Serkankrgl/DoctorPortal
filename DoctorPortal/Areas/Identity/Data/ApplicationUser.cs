5c743b720612790a5ec3db1590d5f008d5a21649
serkankrgl <serkan.kuruoglu26@gmail.com>
2021-12-15 23:54:57 +0300

    Add project files.

---
+++ DoctorPortal/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace DoctorPortal.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName ="nvarchar(100)")]
+        public string Name { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Surname { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Gender { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(5)")]
+        public string IsDoctor { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(11)")]
+        public string TC { get; set; }
+    }
+}
