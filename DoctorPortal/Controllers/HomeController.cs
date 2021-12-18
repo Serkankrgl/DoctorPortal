7379fe8e7984eba85dc3b44a2e6902dec0b9b7b9
Ruhid Ibadli <ruhid.ibadli@gmail.com>
2021-12-18 22:27:17 +0300

    Revert "Plan Değişikliği"
    This reverts commit ccf3b149b86a6e7c129801309399146912d7437d.

--- DoctorPortal/Controllers/HomeController.cs
+++ DoctorPortal/Controllers/HomeController.cs
@@ -29,8 +29,12 @@ namespace DoctorPortal.Controllers
             return View();
         }
 
+        public IActionResult Chats()
+        {
+            return View()
+        }
 
-            
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
