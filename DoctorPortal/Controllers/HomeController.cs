41012061d4a768127ec152d440e41db0e7cb634d
Ruhid Ibadli <ruhid.ibadli@gmail.com>
2021-12-20 18:18:56 +0300

    Post ekleme
    Post ekleme için View ve Controller oluşturuldu.

--- DoctorPortal/Controllers/HomeController.cs
+++ DoctorPortal/Controllers/HomeController.cs
@@ -36,5 +36,11 @@ namespace DoctorPortal.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+    
+        public IActionResult SharePost()
+        {
+            return View();
+        }
+    
     }
 }
