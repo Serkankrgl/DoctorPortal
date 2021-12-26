8f43e5fd145e4bc8a5c5bf816a3cdc6bfc0fe2fc
Ruhid Ibadli <ruhid.ibadli@gmail.com>
2021-12-26 15:25:09 +0300

    API ve Veriler eklendi

---
+++ DoctorPortal/Controllers/PrescriptionsController.cs
@@ -0,0 +1,30 @@
+﻿using DoctorPortal.Data;
+using DoctorPortal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrescriptionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PrescriptionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Prescription>> GetPrescriptions()
+        {
+            return await _context.Prescriptions.ToListAsync();
+        }
+    }
+}
